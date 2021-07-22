f3c4b9c207296800e0d3defdb985e2909960829d
Raphael Queiroz <uroboros.phael@gmail.com>
2021-07-22 19:25:54 -0300

    feat: test build with multiple filenames.

--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -5,10 +5,21 @@ defmodule GenReportTest do
   alias GenReport.Support.ReportFixture
 
   @file_name "gen_report.csv"
+  @file_names [
+    "part_1.csv",
+    "part_2.csv",
+    "part_3.csv"
+  ]
 
   describe "build/1" do
     test "When passing file name return a report" do
       response = GenReport.build(@file_name)
+
+      assert response == ReportFixture.build()
+    end
+
+    test "When passing filenames in a list generate in parallel and return the report" do
+      response = GenReport.build(@file_names)
 
       assert response == ReportFixture.build()
     end
